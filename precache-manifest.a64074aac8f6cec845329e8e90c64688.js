0454f511fb260de3489ba3f976cd2c50bab7e12c
Mark Stoler <mark.stoler.86@gmail.com>
2019-12-26 12:04:28 +0200

    Updates

---
+++ precache-manifest.a64074aac8f6cec845329e8e90c64688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c0a03a866e90a5ed41191733b4e493",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "11deb39ce14d4b3deb30",
+    "url": "/todo-list-react/static/css/main.11ad6086.chunk.css"
+  },
+  {
+    "revision": "3d836d7e4e0fc08072bc",
+    "url": "/todo-list-react/static/js/2.2cca7ef9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list-react/static/js/2.2cca7ef9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11deb39ce14d4b3deb30",
+    "url": "/todo-list-react/static/js/main.c22a06c5.chunk.js"
+  },
+  {
+    "revision": "c987637057df95468283",
+    "url": "/todo-list-react/static/js/runtime-main.2d994b52.js"
+  }
+]);
